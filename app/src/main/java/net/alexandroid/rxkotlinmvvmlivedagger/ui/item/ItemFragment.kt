1b18129eaeb58e146eb5fc72f29a2731e9f1d007
Pulimet <korolev2009@gmail.com>
2018-09-01 17:26:36 +0300

    ItemFragment - Set Image

--- app/src/main/java/net/alexandroid/rxkotlinmvvmlivedagger/ui/item/ItemFragment.kt
+++ app/src/main/java/net/alexandroid/rxkotlinmvvmlivedagger/ui/item/ItemFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_fragment.*
 import net.alexandroid.rxkotlinmvvmlivedagger.R
 import net.alexandroid.utils.mylog.MyLog
 
@@ -13,12 +15,16 @@ class ItemFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val args = ItemFragmentArgs.fromBundle(arguments)
-        MyLog.d("Photo url: ${args.photo.previewURL}")
-
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.item_fragment, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val args = ItemFragmentArgs.fromBundle(arguments)
+        MyLog.d("Photo url: ${args.photo.webformatURL}")
 
+        args.photo.webformatURL.let {
+            Picasso.get().load(args.photo.webformatURL).into(imageView)
+        }
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
